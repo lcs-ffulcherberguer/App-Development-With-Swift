4dc7d03f8797154e2bcfadfa40fd8bf9cc5de954
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-09-30 19:24:10 -0400

    Did exercise 7

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,8 +3,10 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
  */
-
-
+var firstDecimal: Double = 3.14
+var secondDecimal: Double = 8.14
+print (firstDecimal)
+print (secondDecimal)
 /*:
  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
