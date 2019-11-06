51a83ef6f11c8bfa8c1afc791cf6397aa9e17f60
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-11-06 15:32:17 -0500

    Did babe 1 from strings

--- lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ lab/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -3,7 +3,10 @@
  
  Create a `name` constant and assign it a string literal representing your name.
  */
-
+let string1 = "Fernanda"
+let string2 = " Zancheta"
+let string3 = " Fulcherberguer"
+let myString = string1 + string2 + string3 // Fernanda Zancheta Fulcherberguer
 
 /*:
  Create a `favoriteQuote` constant and assign it the following string literal:
@@ -15,12 +18,14 @@
  - callout(Example): If your favorite quote is "The grass is always greener on the other side" the value of `favoriteQuote` should be such that printing `favoriteQuote` results in the following:
     * `My favorite quote is "The grass is always greener on the other side."`
  */
-
-
+let favoriteQuote = "Only dead fish go with the flow."
+print("My favorite quote is \"\(favoriteQuote)\"") // My favorite quote is "Only dead fish go with the flow"
 /*:
  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
  */
 let emptyString = ""
-
+if emptyString.isEmpty {
+    print("There's nothing here")
+}
 
 //: page 1 of 5  |  [Next: Exercise - Concatenation and Interpolation](@next)
